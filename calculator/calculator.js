f84f070defd149f33503165a1477225f1fd1e40f
BlitheBacon <joshuakhagins@gmail.com>
2020-08-05 16:30:26 -0500

    Completed calculator exercises.

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,27 +1,43 @@
-function add () {
-	
+function add (a, b) {
+	return a + b
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b
 }
 
-function sum () {
-	
+function sum (arr) {
+	let sum = 0
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i]
+	}
+	return sum
 }
 
-function multiply () {
-	
+function multiply (arr) {
+	let product = 1
+	for (let i = 0; i < arr.length; i++) {
+		product *= arr[i]
+	}
+	return product
 }
 
-function power() {
-	
+function power(a, b) {
+	return a ** b
 }
 
-function factorial() {
-	
+function factorial(a) {
+	if (a == 0) {
+		return 1
+	}
+	else if (a == 1 || a == 0) {
+		return a * 1
+	}
+	else {
+		return a * factorial(a - 1)
+	}
 }
-
+add(0,0)
 module.exports = {
 	add,
 	subtract,
