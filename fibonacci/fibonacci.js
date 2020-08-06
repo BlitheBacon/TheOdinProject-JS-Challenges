6413279d574671fd48390394a9ca933371fa5c77
BlitheBacon <joshuakhagins@gmail.com>
2020-08-06 10:09:57 -0500

    Completed fibonacci challenge.

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    
+    //Input validation
+    if (n < 0) {
+        return "OOPS"
+    }
+    if (typeof(n) !== 'number') {
+        n = parseInt(n)
+    }
 
+    let next = 0
+    let current = 0
+
+    for (let i = 1; i <= n; i++) {
+        if (i == 1) {
+            current = 1
+        }
+        else if (i % 2 == 0) {
+            next += current
+        }
+        else {
+            current += next
+        }
+    }
+    if (next > current) {
+        return next
+    }
+    else {
+        return current
+    }
 }
-
 module.exports = fibonacci
