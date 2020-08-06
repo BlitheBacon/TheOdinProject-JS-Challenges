d8b21cc4eeeee7115e375b69f3ea3f1e8f5f7ab9
BlitheBacon <joshuakhagins@gmail.com>
2020-08-06 11:44:00 -0500

    Completed findTheOldest challenge

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-let findTheOldest = function() {
+let findTheOldest = function(Array) {
+    return Array.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath)
+        return oldestAge < currentAge ? current : oldest
+    })
+}
 
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear()
+    }
+    return death - birth
 }
 
 module.exports = findTheOldest
