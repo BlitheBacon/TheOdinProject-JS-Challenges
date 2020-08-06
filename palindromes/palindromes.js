e38cff0425df3c4613aec77d9915bb63a6db3e17
BlitheBacon <joshuakhagins@gmail.com>
2020-08-06 12:28:39 -0500

    Completed palidromes challenge.

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function() {
+const palindromes = function(string) {
+    const punctuation = '!"#$%&\'\\s()*+,-./:;<=>?@[\\]^_`{|}~'
+    const toReplace = new RegExp('[' + punctuation + ']', 'g')
+    let fString = string.replace(toReplace, '').toLowerCase()
 
+    for (let i = 0, j = (fString.length - 1); i < fString.length; i++, j--) {
+        if (fString[i] != fString[j]) {
+            return false
+        }
+    }
+    return true
 }
-
 module.exports = palindromes
